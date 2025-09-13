3ecc4db4900849d212a17e07bdd5369e830a5f3d
Pedro Ribeiro <beyluta1@gmail.com>
2025-09-13 08:25:20 +0200

    feat: add nord theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--     },
+--   },
+-- }
+
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
+  { "shaunsingh/nord.nvim", opts = {
+    colorscheme = "nord",
+  } },
 }
