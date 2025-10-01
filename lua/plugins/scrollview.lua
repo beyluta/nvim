77c6893a8413d957690b5502580073e7855f5462
Pedro Ribeiro <beyluta1@gmail.com>
2025-10-01 14:20:11 +0200

    feat: add vscode theme and scrollbar

---
+++ lua/plugins/scrollview.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "dstein64/nvim-scrollview",
+    config = function()
+      require("scrollview").setup({
+        excluded_filetypes = { "nerdtree" },
+        current_only = true,
+        base = "buffer",
+        signs_on_startup = { "all" },
+        diagnostics_severities = { vim.diagnostic.severity.ERROR },
+      })
+    end,
+  },
+}
