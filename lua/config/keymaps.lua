535aa70710a412b54c3256fd7563c4ec7edf149a
Pedro Ribeiro <beyluta1@gmail.com>
2025-12-17 18:51:08 +0100

    feat: add visual studio support for run, build and debug files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 vim.keymap.set("n", "<leader>ls", function()
   require("live-server-nvim").toggle()
 end)
+
+local wk = require("which-key")
+wk.add({
+  { "<leader>v", group = "Visual Studio", icon = "" },
+  { "<leader>vr", "<cmd>Dotnet run<cr>", desc = "Run" },
+  { "<leader>vb", "<cmd>Dotnet build<cr>", desc = "Build" },
+  { "<leader>vd", "<cmd>Dotnet build<cr>", desc = "Debug" },
+})
